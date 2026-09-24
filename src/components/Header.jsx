0c59e9ea5f4c8398d525b3449796d5bdc6cad24e
Muhammad Moeez <moeezahmed1012@gmail.com>
2026-09-24 15:30:53 +0500

    feat: enhance Header component with responsive navigation and toggle functionality

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,33 +1,42 @@
+import { useState } from "react";
 import style from "../css/header.module.css";
 
 function Header() {
+  const [display, setDisplay] = useState(false);
+
   return (
     <header className={style.header}>
       <div className={style.siteLogo}>
         <a href="/">Perfume Store</a>
       </div>
       <nav>
-        <ul>
-          <li>
-            <a href="/">Home</a>
-          </li>
-          <li>
-            <a href="#">Perfumes</a>
-          </li>
-          <li>
-            <a href="#">Collections</a>
-          </li>
-          <li>
-            <a href="#">Track Order</a>
-          </li>
-          <li>
-            <a href="#">About</a>
-          </li>
-          <li>
-            <a href="#">Contact</a>
-          </li>
-        </ul>
+        {display && (
+          <ul>
+            <li>
+              <a href="/">Home</a>
+            </li>
+            <li>
+              <a href="#">Perfumes</a>
+            </li>
+            <li>
+              <a href="#">Collections</a>
+            </li>
+            <li>
+              <a href="#">Track Order</a>
+            </li>
+            <li>
+              <a href="#">About</a>
+            </li>
+            <li>
+              <a href="#">Contact</a>
+            </li>
+          </ul>
+        )}
       </nav>
+      <button className={style.humberger} onClick={() => setDisplay(!display)}>
+        ---
+      </button>
+      {/* <i className="uil uil-bars"></i> */}
     </header>
   );
 }
